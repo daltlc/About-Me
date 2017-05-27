6eb055616ebc567b4a418865ec4f960f564876b7
Dalton Carr <daltonlcarr@yahoo.com>
2017-05-26 18:30:00 -0700

    first three questions written

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var confirmed = confirm('Are you ready to rock and roll!?');
+var one = prompt('Are you a student at Codefellows?');
+var two = prompt('Are you a Seahawks fan?');
+var three = prompt();
+var four = prompt();
 
 if(confirmed === true){
   console.log('The user is ready!');
 } else {
   console.log('The user said they were not ready');
+}
+
+if (one === 'Yes'){
+  console.log('The user attends Codefellows');
+} else {
+  confirm('Join us!');
 }
