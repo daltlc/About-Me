748b48bfd77f7525bf91a4913d1175181e902be0
Dalton Carr <daltonlcarr@yahoo.com>
2017-06-03 21:30:21 -0700

    added 6th question using for loop

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 
-var confirmed = confirm('Are you ready to rock and roll!?');
-
+var confirmed = confirm('Welcome! Ready to learn a little more about me?');
+var correct = 0;
+var wrong = 0;
+var yes = 'yes';
+var y = 'y';
+var no = 'no';
+var n = 'n';
 
 if(confirmed === true){
   console.log('The user is ready!');
@@ -9,42 +14,81 @@ if(confirmed === true){
   console.log('The user said they were not ready');
 }
 
-var one = prompt('Are you a student at Codefellows?');
-one = one.toLowerCase();
 
-if (one === 'yes'){
+
+var one = prompt('Do I attend Codefellows?');
+//one = one.toLowerCase();
+
+if (one.toLowerCase() === yes || one.toLowerCase() === y){
   console.log('The user attends Codefellows');
+  confirm('Correct!');
+  correct++;
+  console.log(correct);
 } else {
-  confirm('Join us!');
+  confirm('Wrong! I sure do!');
+  wrong++;
 }
 
 
-var two = prompt('Are you a Seahawks fan?');
-two = two.toUpperCase();
+var two = prompt('Am I a Seahwaks fan?');
 
-if (two === 'YES'){
-  console.log('Go hawks!');
+if (two.toLowerCase() === no || two.toLowerCase() === n){
+  console.log('Sure ain\'t! Go Bears!');
+  confirm('Correct!');
+  correct++;
+  console.log(correct);
 } else {
-  confirm('BOOOOOOO');
+  confirm('BOOOOOOO, no way!');
+  wrong++;
 }
 
 
 var three = prompt('Have you ever had a Seattle hot dog?');
-three = three.toLowerCase();
 
-if (three === 'yes'){
+if (three.toLowerCase() === yes || three.toLowerCase() === y){
   console.log('Mhmmhmmhmhmhhmmmmm');
+  confirm('Better have!');
+  correct++;
+  console.log(correct);
 } else {
-  confirm('WHAT?!');
+  confirm('WHAT?! Leave now and go get one..');
+  wrong++;
 }
 
 
 var four = prompt('Thanks for checking out my page! Did you have fun?!');
-four = four.toLowerCase();
 
-if (four === 'yes'){
+if (four.toLowerCase() === yes || four.toLowerCase() === y){
   console.log('The user indeed had fun');
-  confirm('Have a good one!')
+  confirm('Have a good one!');
+  correct++;
+  console.log(correct);
 } else {
   confirm('Too bad so sad');
+  wrong++;
 }
+
+var vaild = false;
+var userGuess;
+
+for (var valid = false; vaild < 4; vaild++){
+    userGuess = prompt("How old do you think I am? You have 4 tries!");
+    if (userGuess === '22') {
+        valid = true;
+        confirm('Correct!')
+        correct++;
+    } else {
+        console.log("That number is invalid! Please enter a another number.");
+        confirm('Try again!')
+        wrong++;
+    }
+}
+
+var states;
+var valid2 = false;
+states = ['Nevada','California','Oregon','Utah'];
+
+
+
+console.log('Correct:' + correct);
+console.log('Wrong:' + wrong);
